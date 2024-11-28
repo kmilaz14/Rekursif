0253385c13ba11dc9371bc5b7f78b53bd576d02a
kmilaz14 <kamilazhwa@gmail.com>
2024-11-28 12:13:27 +0800

    tugas 1

---
+++ Tugas1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    static void descendingRekursif(int n){
+        if (n< 0){
+            return;
+        }
+        System.out.print(n + " ");
+        descendingRekursif(n-1);
+    }
+
+    static void descendingIteratif(int n){
+        for (int i=n; i>=0; i--){
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Menggunakan fungsi rekursif: ");
+        descendingRekursif(n);
+        System.out.println("\nMenggunakan fungsi iteratif: ");
+        descendingIteratif(n);
+    }
+}
