3420aaa058265424982c527a06aaf2ee3164c36f
kmilaz14 <kamilazhwa@gmail.com>
2024-11-28 12:26:42 +0800

    tugas 3

---
+++ Tugas3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Tugas3 {
+    static int fibonacci(int bulan){
+        if (bulan == 1 || bulan==2) {
+            return 1;
+        }
+        return fibonacci(bulan-1) + fibonacci(bulan-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan bulan ke-n: ");
+        int bulan = sc.nextInt();
+
+        int totalPasangan = fibonacci(bulan);
+        System.out.println("Jumlah total pasangan marmut pada bulan ke-" + bulan + " adalah: " + totalPasangan);
+    }
+}
